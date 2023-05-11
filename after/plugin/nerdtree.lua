0ed270d0ee5210e7870b3379551da4a566be83f2
pggodoy <pigodoy1@miuandes.cl>
2023-05-11 13:29:18 -0400

    Update nerdtree.lua
    Now it closes when is the last thing in a tab or nvim

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("VimEnter", {
     pattern="*",
     command="NERDTree | if argc() > 0 || exists('s:std_in') | wincmd p | endif "
 })
+vim.api.nvim_create_autocmd("BufEnter" , {
+    pattern="*",
+    command="if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif"
+})
+vim.api.nvim_create_autocmd("BufEnter" , {
+    pattern="*",
+    command="if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif"
+})
