a57493fcdf57bd40ba78b73aaeced9d7a510887a
pggodoy <pigodoy1@miuandes.cl>
2023-05-11 17:51:24 -0400

    Added nvim-autopairs

--- lua/pggodoy/packer.lua
+++ lua/pggodoy/packer.lua
@@ -42,4 +42,9 @@ return require('packer').startup(function(use)
 
     use('preservim/nerdtree')
 
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
   end)
