f0bb83021a7bb91a510b95d0417c90b61b01c599
pggodoy <pigodoy1@miuandes.cl>
2023-05-11 13:17:31 -0400

    Formatting in NERDTree.lua

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -2,6 +2,6 @@ vim.g.NERDTreeShowHidden = 1
 
 vim.api.nvim_create_autocmd("StdinReadPre", {pattern="*", command="let s:std_in=1"})
 vim.api.nvim_create_autocmd("VimEnter", {
-                                pattern="*",
-                                command="NERDTree | if argc() > 0 || exists('s:std_in') | wincmd p | endif "
-                            })
+    pattern="*",
+    command="NERDTree | if argc() > 0 || exists('s:std_in') | wincmd p | endif "
+})
