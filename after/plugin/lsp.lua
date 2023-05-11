792967ebd2ff40870c983a795a9747bd0862eeee
pggodoy <pigodoy1@miuandes.cl>
2023-05-11 13:17:58 -0400

    Update lsp.lua
    Enter to autocomplete and <gd> to go to definition

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,11 +9,19 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 local cmp = require('cmp')
 local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+
+end)
 
 lsp.setup()
